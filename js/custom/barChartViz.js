3353d838a3a9cd812a8838bd734ef9018ee0fbaf
Peter Gathua <petergathua@fas.harvard.edu>
2015-05-04 00:11:49 -0400

    Merge branch 'master' of https://github.com/campussecurity/campussecurity.github.io
    # Conflicts:
    #	js/custom/barChartViz.js

--- js/custom/barChartViz.js
+++ js/custom/barChartViz.js
@@ -8,7 +8,7 @@ BarChartViz = function(_theDiv,_eventHandler,_year,_crimeCategory){
         'AK',
         'AL',
         'AR',
-        'AS',
+        //'AS',
         'AZ',
         'CA',
         'CO',
@@ -52,7 +52,7 @@ BarChartViz = function(_theDiv,_eventHandler,_year,_crimeCategory){
         'TX',
         'UT',
         'VA',
-        'VI',
+        //'VI',
         'VT',
         'WA',
         'WI',
@@ -73,16 +73,17 @@ BarChartViz.prototype.init = function(_theDiv,_height,_width)
     that.states.forEach(function(d,i){
         var stateData = yearBucket["state"+d];
         bardata.push({
+
             state:d,
-            count:stateData.crimeCounts[that.crimeCateogry]
+            count: stateData.crimeCounts[that.crimeCateogry]
         });
     });
 
-    var margin = { top: 30, right: 30, bottom: 40, left:50 }
+    console.log(bardata)
+    var margin = { top: 30, right: 5, bottom: 40, left:60 }
 
     var height = that.height  - margin.top - margin.bottom,
         width = that.width - margin.left - margin.right,
-        barWidth = 50,
         barOffset = 5;
 
     that.svg = d3.select('#'+ _theDiv).append('svg')
@@ -110,109 +111,116 @@ BarChartViz.prototype.init = function(_theDiv,_height,_width)
         .domain([0, bardata.length*.33, bardata.length*.66, bardata.length])
         .range(['#B58929','#C61C6F', '#268BD2', '#85992C'])
 
-        var yScale = d3.scale.linear()
-            .domain(extent)
-            .range([0, height]);
+    var yScale = d3.scale.linear()
+        .domain(extent)
+        .range([0, height]);
 
-        var xScale = d3.scale.ordinal()
-            .domain(d3.range(0, bardata.length))
-            .rangeBands([0, width], 0.2)
+    var xScale = d3.scale.ordinal()
+        .domain(that.states)
+        .rangeBands([0, width], 0.2)
 
-        var hAxis = d3.svg.axis()
-            .scale(xScale)
-            .orient('bottom')
-            .tickValues(xScale.domain().filter(function(d, i) {
-                return !(i % (bardata.length/5));
-            }))
+    var hAxis = d3.svg.axis()
+        .scale(xScale)
+        .orient('bottom');
 
-        var vAxisGroup = that.svg.append("g")
-            .attr("class",'axis')
-            .attr("transform",
-            "translate("+ 30 + ","+margin.top+")").call(vAxis)
+    /*.tickValues(xScale.domain().filter(function(d, i) {
+     return d.state;
+     }))*/
 
-
-        var hAxisGroup = that.svg.append("g")
-            .attr("class",'axis')
-            .attr("transform",
-            //"translate("+ 0 + ","+height + margin.top + margin.bottom+")").call(hAxis)
-            "translate(30,280)").call(hAxis)
-
-        var tooltip = d3.select('body').append('div')
-            .style('position', 'absolute')
-            .style('padding', '0 5px')
-            .style('font-size', '10px')
-            .style('background', 'white')
-            .style('opacity', 0)
-
-        var myChart = that.svg
-            .append('g')
-            .attr('transform', 'translate('+ margin.left +', '+ margin.top +')')
-            .selectAll('rect')
-            .data(bardata)
-            .enter()
-            .append('rect')
-            .style('fill', function(d,i) {
-                return 'blue';
-            })
-            .attr('width', xScale.rangeBand())
-            .attr('x', function(d,i) {
-                console.log(xScale(d.count))
-                return xScale(d.count);
-            })
-            .attr('height', 0)
-            .attr('y', height)
-
-            .on('mouseover', function(d) {
-
-                tooltip.transition()
-                    .style('opacity', .9)
-
-                tooltip.html(d.count)
-                    .style('left', (d3.event.pageX - 35) + 'px')
-                    .style('top',  (d3.event.pageY - 30) + 'px')
+    var vAxisGroup = that.svg.append("g")
+        .attr("class",'stateAxes')
+        .attr("transform",
+        "translate("+ 49 + ","+margin.top+")").call(vAxis)
 
 
-                tempColor = this.style.fill;
-                d3.select(this)
-                    .style('opacity', .2)
-            })
+    var hAxisGroup = that.svg.append("g")
+        .attr("class",'stateAxes')
+        .attr("transform",
+        //"translate("+ 0 + ","+height + margin.top + margin.bottom+")").call(hAxis)
+        "translate(40, "+ (margin.top + height) +")").call(hAxis)
+        .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("dx", "-.8em")
+        .attr("dy", (1.5 * xScale.rangeBand()))
+        .attr("transform", function(d) {
+            return "rotate(-90)"
+        });
 
-            .on('mouseout', function(d) {
-                d3.select(this)
-                    .style('opacity', 1)
-                    .style('fill', tempColor)
-            })
+    var tooltip = d3.select('body').append('div')
+        .style('position', 'absolute')
+        .style('padding', '0 5px')
+        .style('font-size', '10px')
+        .style('background', 'white')
+        .style('opacity', 0)
 
-        myChart.transition()
-            .attr('height', function(d) {
-                return yScale(d.count);
-            })
-            .attr('y', function(d) {
-                return height - yScale(d.count);
-            })
-            .delay(function(d, i) {
-                return i * 20;
-            })
-            .duration(1000)
-            .ease('elastic')
+    var myChart = that.svg
+        .append('g')
+        .attr('transform', 'translate('+ margin.left +', '+ margin.top +')')
+        .selectAll('rect')
+        .data(bardata)
+        .enter()
+        .append('rect')
+        .style('fill', function(d,i) {
+            return 'blue';
+        })
+        .attr('width', xScale.rangeBand())
+        .attr('x', function(d,i) {
+            return xScale(d.state);
+        })
+        .attr('height', 0)
+        .attr('y', height)
 
-        var vGuide = myChart.append('g')
-        vAxis(vGuide)
-        vGuide.attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')')
-        vGuide.selectAll('path')
-            .style({ fill: 'none', stroke: "#000"})
-        vGuide.selectAll('line')
-            .style({ stroke: "#000"})
+        .on('mouseover', function(d) {
+
+            tooltip.transition()
+                .style('opacity', .9)
+
+            tooltip.html(d.count)
+                .style('left', (d3.event.pageX - 35) + 'px')
+                .style('top',  (d3.event.pageY - 30) + 'px')
+
+
+            tempColor = this.style.fill;
+            d3.select(this)
+                .style('opacity', .2)
+        })
+
+        .on('mouseout', function(d) {
+            d3.select(this)
+                .style('opacity', 1)
+                .style('fill', tempColor)
+        })
+
+    myChart.transition()
+        .attr('height', function(d) {
+            return yScale(d.count);
+        })
+        .attr('y', function(d) {
+            return height - yScale(d.count);
+        })
+        .delay(function(d, i) {
+            return i * 20;
+        })
+        .duration(1000)
+        .ease('elastic')
+
+    var vGuide = myChart.append('g')
+    vAxis(vGuide)
+    vGuide.attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')')
+    vGuide.selectAll('path')
+        .style({ fill: 'none', stroke: "#000"})
+    vGuide.selectAll('line')
+        .style({ stroke: "#000"})
 
 
 
-        var hGuide = myChart.append('g')
-        hAxis(hGuide)
-        hGuide.attr('transform', 'translate(' + margin.left + ', ' + (height + margin.top) + ')')
-        hGuide.selectAll('path')
-            .style({ fill: 'none', stroke: "#000"})
-        hGuide.selectAll('line')
-            .style({ stroke: "#000"})
+    var hGuide = myChart.append('g')
+    hAxis(hGuide)
+    hGuide.attr('transform', 'translate(' + margin.left + ', ' + (height + margin.top) + ')')
+    hGuide.selectAll('path')
+        .style({ fill: 'none', stroke: "#000"})
+    hGuide.selectAll('line')
+        .style({ stroke: "#000"})
     //});
 
 }
